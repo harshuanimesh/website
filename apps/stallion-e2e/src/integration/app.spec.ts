91a1728ecb04f08ce13536b5e14c6cbff0b62c67
ANIMESH HARSH <harshuanimesh@gmail.com>
2022-04-01 14:28:44 +0530

    interface app changed to stallion

--- apps/stallion-e2e/src/integration/app.spec.ts
+++ apps/stallion-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('interface', () => {
+describe('stallion', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('interface', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome interface');
+    getGreeting().contains('Welcome stallion');
   });
 });
--- apps/stallion/jest.config.js
+++ apps/stallion/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'interface',
+  displayName: 'stallion',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/interface',
+  coverageDirectory: '../../coverage/apps/stallion',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
